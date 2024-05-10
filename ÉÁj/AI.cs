1f1f472c7da53e5b0763c03a42a548e209d934b0
Majoros-Peter <70958388+Majoros-Peter@users.noreply.github.com>
2024-05-10 12:13:06 +0200

    Add files via upload

---
+++ ÉÁj/AI.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using static ÉÁj.CONSTS;
+
+namespace ÉÁj
+{
+    public class AI
+    {
+        public static List<AI> EAjok = new();
+        public static List<int> Foglalt = new();
+
+        private HashSet<Point> lephet = new();
+
+        public Point Coord { get; private set; }
+        public int X => Coord.X;
+        public int Y => Coord.Y;
+
+        public AI(int x, int y, int id)
+        {
+            Coord = new(x, y);
+            Foglalt.Add(id);
+
+            EAjok.Add(this);
+        }
+
+        private HashSet<Point> Lephet()
+        {
+            HashSet<Point> result = new();
+
+            if(X > 0)
+                result.Add(new (X-1, Y));
+            if(Y > 0)
+                result.Add(new (X, Y-1));
+            if(X < OSZLOPOKSZAMA)
+                result.Add(new (X+1, Y));
+            if(Y < SOROKSZAMA)
+                result.Add(new (X, Y+1));
+
+            //Where(G => G == nincs_babu)
+
+            return result;
+        }
+
+        public Point Lepes()
+        {
+            var lephet = Lephet();
+
+            return Rand(lephet);
+        }
+
+        private Point Rand(ISet<Point> collection) => collection.ElementAt(RAND.Next(collection.Count));
+    }
+}
---
+++ ÉÁj/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static ÉÁj.CONSTS;
+
+namespace ÉÁj
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Letrehoz();
+        }
+
+        private void Letrehoz()
+        {
+            for (int i = 0; i < OSZLOPOKSZAMA; i++)
+                grid.RowDefinitions.Add(new());
+
+
+            for (int i = 0; i < OSZLOPOKSZAMA; i++)
+                grid.ColumnDefinitions.Add(new());
+
+
+            for (int i = 0; i < LEPKEDOKSZAMA; i++)
+            {
+            General:
+                int koord = RAND.Next(OSZLOPOKSZAMA * SOROKSZAMA);
+                if (AI.Foglalt.Contains(koord))
+                    goto General;
+
+                AI ai = new(koord % OSZLOPOKSZAMA, koord / OSZLOPOKSZAMA, koord);
+
+                Button btn = new()
+                {
+                    Content = koord
+                };
+
+                grid.Children.Add(btn);
+
+                Grid.SetColumn(btn, ai.X);
+                Grid.SetRow(btn, ai.Y);
+            }
+
+            for (int i = 0; i < LEKEKSZAMA; i++)
+            {
+            General:
+                int koord = RAND.Next(OSZLOPOKSZAMA * SOROKSZAMA);
+                if (AI.Foglalt.Contains(koord))
+                    goto General;
+
+                Ellipse Juk = new()
+                {
+                    Fill = new SolidColorBrush(Colors.Coral)
+                };
+
+                grid.Children.Add(Juk);
+                Grid.SetColumn(Juk, koord % OSZLOPOKSZAMA);
+                Grid.SetRow(Juk, koord / OSZLOPOKSZAMA);
+            }
+        }
+
+        private void Lepked()
+        {
+            AI.EAjok.ForEach(ai =>
+            {
+
+            });
+        }
+    }
+
+    public static class CONSTS
+    {
+        public const int OSZLOPOKSZAMA = 30;
+        public const int SOROKSZAMA = 15;
+        public const int LEKEKSZAMA = 10;
+        public const int LEPKEDOKSZAMA = 150;
+
+        public static readonly Random RAND = new();
+    }
+}
